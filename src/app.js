2c437ec26c6d9a0403476f100c48d7b9ef77d273
rdekeyser <robin.dekeyser@crossroad.be>
2020-05-04 22:36:16 +0200

    Completed module

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.post('/transfer', (req,res) => {
 app.get('/payment', (req,res) => res.render('payment', {account:accounts.credit}));
 
 app.post('/payment', (req,res) => {
-	accounts.credit.balance -= parseInt(accounts.credit.balance) - parseInt(req.body.amount,10)
+	accounts.credit.balance -= parseInt(req.body.amount,10)
 	accounts.credit.available += parseInt(req.body.amount,10);
 	const accountsJSON = JSON.stringify(accounts, null, 4);
 	fs.writeFileSync(path.join(__dirname, 'json/accounts.json'),accountsJSON, 'utf8');
